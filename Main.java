0d6c50b6cd2cd8f56e4a688d34b1ad83d15d9047
Alan Filho <devaneiosdoalan@gmail.com>
2024-02-24 23:52:28 -0300

    second

--- Main.java
+++ Main.java
@@ -8,9 +8,12 @@ public class Main {
         String nome = "Alan";
         int idade = 19;
         double renda = 4000.0;
+
         System.out.printf("%.2f%n", x);
         System.out.printf("%.4f%n", x);
+
         Locale.setDefault(Locale.US);
+
         System.out.println(y);
         System.out.println("Resultado = " + x + " m");
         System.out.printf("Resultado = %.2f metros%n", x);
