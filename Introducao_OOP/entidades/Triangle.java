e830e19719a2cb8d55bcf08e1ca57a4d041753ee
Alan Filho <devaneiosdoalan@gmail.com>
2024-03-01 00:18:14 -0300

    aula_OOP

---
+++ Introducao_OOP/entidades/Triangle.java
@@ -0,0 +1,13 @@
+package Introducao_OOP.entidades;
+
+public class Triangle {
+
+    public double a;
+    public double b;
+    public double c;
+
+    public double area() { // como já temos as instancias declaradas, nao precisamos digitar os parâmetros que iriamos utilizar na funcao
+        double p = (a + b + c) / 2.0;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+}
